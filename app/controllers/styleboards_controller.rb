9a5163e90a914819b02bd2488e221327ec8713b1
LesleyMdCampo <lesliz32@gmail.com>
2014-01-20 11:01:17 -0600

    Add styleboard controller actions and views.

--- app/controllers/styleboards_controller.rb
+++ app/controllers/styleboards_controller.rb
@@ -69,6 +69,6 @@ class StyleboardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def styleboard_params
-      params.require(:styleboard).permit(:description, :image, :place, :name)
+      params[:styleboard]
     end
 end
